d99658b87c2b370c1053681e6662bcc515151f43
dingyanxu <dingyanxu@mova-tech.com>
2026-05-16 22:32:45 +0800

    [add]:add func testB

--- main.cc
+++ main.cc
@@ -4,7 +4,13 @@ void testA() {
   std::cout << "this is a" << std::endl;
 }
 
+void testB() {
+  // this is a comment
+  std::cout << "this is b" << std::endl;
+}
+
 int main(int agrc, char** agrv) {
   testA();
+  testB();
   return 0;
 }
